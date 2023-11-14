c487904249555ab18b7f9e445ed397ab7ec4f36a
devenlf <lifei01@hesaitech.com>
2023-11-14 22:40:03 +0800

    配置脚本

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // root:'./src', 
-  // base:'./',
+  base:'./',
   // build:{
   //   outDir:'../docs'
   // },
