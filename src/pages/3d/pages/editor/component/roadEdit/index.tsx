d55be738d10c89420ac68a755bebbe4b053d5c7e
devenlf <lifei01@hesaitech.com>
2023-12-13 11:47:35 +0800

    开发

--- src/pages/3d/pages/editor/component/roadEdit/index.tsx
+++ src/pages/3d/pages/editor/component/roadEdit/index.tsx
@@ -1,14 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   Button,
   Collapse,
   CollapseProps,
   Form,
-  Image,
   InputNumber,
+  Radio,
+  Space,
 } from "antd";
 import { listImg } from "./config";
+import { useState } from "react";
 const RoadEdit = () => {
+  const [value, setValue] = useState(1);
+  const onFinish = (value: object) => {
+    console.log(value, "value");
+  };
+
+  const onChange = (val: any) => {
+    setValue(val?.target.value)
+  };
   const items: CollapseProps["items"] = [
+    {
+      key: "material",
+      label: "材质",
+      children: (
+        <Radio.Group onChange={onChange} value={value}>
+          <Space direction="vertical">
+            {listImg.map(({ type, src }) => (
+              <Radio value={type}>
+                <div className="materialImg">
+                  <img src={src} />
+                </div>
+              </Radio>
+            ))}
+          </Space>
+        </Radio.Group>
+      ),
+    },
     {
       key: "size",
       label: "尺寸",
@@ -21,50 +49,26 @@ const RoadEdit = () => {
           wrapperCol={{ flex: 1 }}
           colon={false}
           style={{ maxWidth: 600 }}
+          onFinish={onFinish}
         >
-          <Form.Item
-            label="宽度"
-            name="x"
-            rules={[{ required: true }]}
-          >
+          <Form.Item label="宽度" name="x" rules={[{ required: true }]}>
             <InputNumber />
           </Form.Item>
 
-          <Form.Item
-            label="长度"
-            name="y"
-            rules={[{ required: true }]}
-          >
+          <Form.Item label="长度" name="y" rules={[{ required: true }]}>
             <InputNumber />
           </Form.Item>
 
-          <Form.Item
-            label="高度"
-            name="z"
-            rules={[{ required: true }]}
-          >
+          <Form.Item label="高度" name="z" rules={[{ required: true }]}>
             <InputNumber />
           </Form.Item>
 
           <Form.Item label=" ">
             <Button type="primary" htmlType="submit">
-              生成
+              设置
             </Button>
           </Form.Item>
         </Form>
-      ),
-    },
-    {
-      key: "material",
-      label: "材质",
-      children: (
-        <div>
-          {listImg.map(({ type, src }) => (
-            <div className="materialImg" key={type}>
-              <img src={src}  />
-            </div>
-          ))}
-        </div>
       ),
     },
   ];
